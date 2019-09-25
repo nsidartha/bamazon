0bc3d11a7742da99af7c1e9e24fb13870f53108b
Sid <Uverrock@live.com>
2019-09-24 18:53:12 -0700

    data

---
+++ bamazon_schema.sql
@@ -0,0 +1,24 @@
+
+CREATE DATABASE bamazon;
+USE bamazon;
+
+CREATE TABLE products (
+  item_id int AUTO_INCREMENT,
+  product_name varchar(30) NOT NULL,
+  department_name varchar(30) NOT NULL,
+  price DECIMAL(5,2) NOT NULL,
+  stock_quantity int NOT NULL,
+  PRIMARY KEY(item_id)
+);
+
+
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ();
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ();
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ();
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ();
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ();
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ();
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ();
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ();
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ();
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ();
